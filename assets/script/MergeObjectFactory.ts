3c23fc662288b0056849576051980b28e2d34731
JiAn Lee <32667619+choring0989@users.noreply.github.com>
2023-01-09 12:46:44 +0900

    fix functions

--- assets/script/MergeObjectFactory.ts
+++ assets/script/MergeObjectFactory.ts
@@ -1,4 +1,4 @@
-import { _decorator, Node, Component, utils } from 'cc';
+import { _decorator, Node, Component, utils, Vec3 } from 'cc';
 import evolutionData from '../data/evolution.json';
 import { MapData } from './MapData';
 import { ObjectFactory } from './ObjectFactory';
@@ -55,10 +55,9 @@ export class MergeObjectFactory {
             let blockData = currentMap[objData[1]];
             if (blockData && blockData[0] !== '') {
                 const obj = ObjectFactory.get(objData[0]);
-                const x = Math.floor(objData[1] % this.mapSetting.maxRow);
-                const y = objData[1] === 0 ? 0 : Math.floor(objData[1] / this.mapSetting.maxCol);
+                const position = this.getPositionXY(objData[1]);
                 const objComponent = obj.getComponent(Mergeable);
-                obj.setPosition(x + this.mapSetting.startRow, y + this.mapSetting.startCol, 0);
+                obj.setPosition(position.x + this.mapSetting.startRow, position.y + this.mapSetting.startCol, 0);
                 this.mergeLayer.addChild(obj);
                 if (objComponent) {
                     this.mObject.push(objComponent);
@@ -94,24 +93,22 @@ export class MergeObjectFactory {
 
     createRandomObject(): boolean {
         // 생성 역연산 해서 이미 오브젝트가 존재하는 블럭 번호를 가져옴
-        let existN = this.mObject.map((mergeable: Mergeable) =>
-            mergeable.node.getPosition().x - this.mapSetting.startRow
-            + (mergeable.node.getPosition().y - this.mapSetting.startCol) * this.mapSetting.maxCol);
+        let existN = this.mObject.map((mergeable: Mergeable) => this.getPositionNth(mergeable.node.getPosition().x, mergeable.node.getPosition().y));
         const rangeN = Array(this.map.currentMapData.length - 1).fill(0).map((v, i) => i + 1);
         rangeN.push(0); // 위 코드로 0이 안들어가서 강제 삽입
+
         // 차집합계산으로 오브젝트가 존재하지 않는 블럭을 찾음
         const itemsN = rangeN.filter(x => existN.indexOf(x) === -1);
         itemsN.sort(() => Math.random() - 0.5); // 랜덤으로 섞음
-        for(let i = 0; i < itemsN.length; i++) {
+        for (let i = 0; i < itemsN.length; i++) {
             // 빈 블럭이 아닌 곳의 번호를 가져와서 배치함
-            let blockData = this.map.currentMapData[itemsN[i]];
-            if (blockData && blockData[0] !== '') {
-                const x = Math.floor(itemsN[i] % this.mapSetting.maxRow);
-                const y = itemsN[i] === 0 ? 0 : Math.floor(itemsN[i] / this.mapSetting.maxCol);
-                this.createMergedObject(x + this.mapSetting.startRow, y + this.mapSetting.startCol, Utils.randomPickInArray(Object.keys(evolutionData)));
+            if (!this.isNullBlock(itemsN[i])) {
+                const position = this.getPositionXY(itemsN[i]);
+                this.createMergedObject(position.x + this.mapSetting.startRow, position.y + this.mapSetting.startCol, Utils.randomPickInArray(Object.keys(evolutionData)));
                 return true;
             }
         }
+
         return false;
     }
 
@@ -123,6 +120,24 @@ export class MergeObjectFactory {
             }
         }
         return false;
+    }
+
+    private isNullBlock(n?: number, x?: number, y?: number) {
+        let blockData = x || y != null ?
+            this.map.currentMapData[this.getPositionNth(x, y)] : this.map.currentMapData[n];
+        if (blockData && blockData[0] !== '') {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    private getPositionNth(x: number, y: number): number {
+        return x - this.mapSetting.startRow + (y - this.mapSetting.startCol) * this.mapSetting.maxCol;
+    }
+
+    private getPositionXY(n: number): Vec3 {
+        return new Vec3(Math.floor(n % this.mapSetting.maxRow), n === 0 ? 0 : Math.floor(n / this.mapSetting.maxCol), 0);
     }
 
     getNextPrefabEvolution(key: string) {
